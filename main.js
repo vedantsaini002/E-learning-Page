2ae34c69639d9eacb83387052e74fdf89011681f
Vedant Saini <vedantsaini456@gmail.com>
2026-06-22 13:48:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,51 @@
 const scrollRevealOption = {
-    distance: "50px",
-    origin: "bottom",
-    duration: 1000,
-  };
-  
-  ScrollReveal().reveal(".header__image img", {
-    ...scrollRevealOption,
-    origin: "right",
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 300,
+});
+
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 600,
+});
+
+ScrollReveal().reveal(".header__content form", {
+  ...scrollRevealOption,
+  delay: 900,
+});
+
+ScrollReveal().reveal(".card", {
+  ...scrollRevealOption,
+  interval: 200,
+});
+
+ScrollReveal().reveal(".testimonial", {
+  ...scrollRevealOption,
+  interval: 200,
+});
+
+ScrollReveal().reveal(".contact__form", {
+  ...scrollRevealOption,
+  delay: 300,
+});
+
+// Contact Form Alert
+const form = document.querySelector(".contact__form");
+
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Thank you! Your message has been submitted.");
+    form.reset();
   });
-  ScrollReveal().reveal(".header__content h1", {
-    ...scrollRevealOption,
-    delay: 500,
-  });
-  ScrollReveal().reveal(".header__content p", {
-    ...scrollRevealOption,
-    delay: 1000,
-  });
-  ScrollReveal().reveal(".header__content form", {
-    ...scrollRevealOption,
-    delay: 1500,
-  });
+}
